430a71d6719a6ececd6b9302830fa87c3ee74ee2
Josana-Kla <josana.0205@gmail.com>
2022-12-16 16:56:55 -0300

    config: express, express.json, cors, port and routes execution in index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+
+import signUpRouter from './routers/signup.routers.js';
+import signInRouter from './routers/signin.routers.js';
+import urlRouter from './routers/urls.routers.js';
+import userRouter from './routers/users.routers.js';
+import rankingRouter from './routers/ranking.routers.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// ROUTES:
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(urlRouter);
+app.use(userRouter);
+app.use(rankingRouter);
+
+app.listen(4000, () => console.log(`Executando na porta 4000...`));
