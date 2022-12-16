3861f5fae9589d3021c25b462b977c5e6d48a4b6
Josana-Kla <josana.0205@gmail.com>
2022-12-16 16:39:50 -0300

    route: created users router

---
+++ src/routers/users.routers.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getMyInformations } from '../controllers/users.controller.js';
+
+const router = express.Router();
+
+router.get("/users/me", getMyInformations);
+
+export default router;
