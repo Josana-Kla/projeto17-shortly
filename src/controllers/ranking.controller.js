7bdcef01b1edc4950d132a3e1a58fa16fd4739fc
Josana-Kla <josana.0205@gmail.com>
2022-12-23 17:47:21 -0300

    fix: get rankings in ranking controller

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,7 @@ import connection from '../database/database.js';
 async function getRankings(req, res) {
     try {
         const { rows } = await connection.query(`
-            SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", visits."qtdVisits" AS "visitCount" FROM users LEFT JOIN urls ON urls."userId" = users.id JOIN visits ON visits."urlId" = urls.id GROUP BY users.id, visits."qtdVisits";
+            SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", visits."qtdVisits" AS "visitCount" FROM users LEFT JOIN urls ON urls."userId" = users.id JOIN visits ON visits."urlId" = urls.id GROUP BY users.id, visits."qtdVisits" ORDER BY "visitCount" DESC, "linksCount" LIMIT 10;
         `);
 
         return res.status(200).send(rows);
